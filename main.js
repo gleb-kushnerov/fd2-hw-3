02e3b06afc4ed0acef5a23014736c4093c2fabf7
Che1sea57 <38821256+Che1sea57@users.noreply.github.com>
2019-03-28 00:03:11 +0300

    частично написал метод toggle

--- main.js
+++ main.js
@@ -45,22 +45,51 @@
 
     let criteriaArr = [number, array, string];
 
-
-
-
     class Validation {
         constructor(criteria) {
-            this.criteria = criteria;
+            this.allCriteria = criteria;
+            this.customCriteria = new Set(criteria);
         };
-
         toggleValidator(name, state) {
-
+            let result = new Set();
+            for (let criteria of [...this.customCriteria]) {
+                if (criteria.name === name) {
+                    result.add('true');
+                } else {
+                    result.add('false')
+                }
+            }
+            if (result.has('true')) {
+                for (let criteria of [...this.customCriteria]) {
+                    if (criteria.name === name) {
+                        this.customCriteria.delete(criteria);
+                    }
+                }
+            } else  {
+                for (let criteria of this.allCriteria) {
+                    if (criteria.name === name) {
+                        this.customCriteria.add(criteria)
+                    }
+                }
+            }
+            // if (this.allCriteria.length === this.customCriteria.size) {
+            //     for (let criterion of [...this.customCriteria]) {
+            //         if (criterion.name === name && !state) {
+            //             this.customCriteria.delete(criterion);
+            //         }
+            //     }
+            // } else {
+            //     for (let criterion of this.allCriteria) {
+            //         if (criterion.name === name && state !== false) {
+            //             this.customCriteria.add(criterion);
+            //         }
+            //     }
+            // }
         }
-
         validate(value) {
             let result = [],
                 errorsMap = new Map;
-            for (let criterion of this.criteria) {
+            for (let criterion of [...this.customCriteria]) {
                 if (criterion.check(value)) {
                     result.push('true');
                 } else {
@@ -83,6 +112,16 @@
     }
 
     let validObj = new Validation(criteriaArr);
-    console.log(validObj.validate('hello'));
+    validObj.toggleValidator('Array');
+    validObj.toggleValidator('Array');
+    validObj.toggleValidator('Array');
+    validObj.toggleValidator('Number');
+    validObj.toggleValidator('Number');
+
+
+
+
+
+    console.log(validObj.customCriteria);
 
 }());
