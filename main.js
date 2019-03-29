6c9ad252053bf9f674e830d0c873f321dea31225
Che1sea57 <38821256+Che1sea57@users.noreply.github.com>
2019-03-29 19:33:42 +0300

    закончил метод toggle

--- main.js
+++ main.js
@@ -1,50 +1,6 @@
 (function () {
     'use strict';
 
-    let number = {
-        name: 'Number',
-        check(value) {
-            return Number.isFinite(value);
-        },
-        message(value) {
-            if (this.check(value)) {
-                return `${value} is number`
-            } else {
-                return `${value} is not a number`
-            }
-        }
-    };
-
-    let array = {
-        name: 'Array',
-        check(value) {
-            return Array.isArray(value);
-        },
-        message(value) {
-            if (this.check(value)) {
-                return `${value} is array`
-            } else {
-                return `${value} is not array`
-            }
-        }
-    };
-
-    let string = {
-        name: 'String',
-        check(value) {
-            return typeof value === 'string';
-        },
-        message(value) {
-            if (this.check(value)) {
-                return `${value} is string`
-            } else {
-                return `${value} is not a string`
-            }
-        }
-    };
-
-    let criteriaArr = [number, array, string];
-
     class Validation {
         constructor(criteria) {
             this.allCriteria = criteria;
@@ -61,30 +17,17 @@
             }
             if (result.has('true')) {
                 for (let criteria of [...this.customCriteria]) {
-                    if (criteria.name === name) {
+                    if (criteria.name === name && !state) {
                         this.customCriteria.delete(criteria);
                     }
                 }
             } else  {
                 for (let criteria of this.allCriteria) {
-                    if (criteria.name === name) {
+                    if (criteria.name === name && state !== false) {
                         this.customCriteria.add(criteria)
                     }
                 }
             }
-            // if (this.allCriteria.length === this.customCriteria.size) {
-            //     for (let criterion of [...this.customCriteria]) {
-            //         if (criterion.name === name && !state) {
-            //             this.customCriteria.delete(criterion);
-            //         }
-            //     }
-            // } else {
-            //     for (let criterion of this.allCriteria) {
-            //         if (criterion.name === name && state !== false) {
-            //             this.customCriteria.add(criterion);
-            //         }
-            //     }
-            // }
         }
         validate(value) {
             let result = [],
@@ -111,17 +54,51 @@
         };
     }
 
+
+    let number = {
+            name: 'Number',
+            check(value) {
+                return Number.isFinite(value);
+            },
+            message(value) {
+                if (this.check(value)) {
+                    return `${value} is number`
+                } else {
+                    return `${value} is not a number`
+                }
+            }
+        },
+        array = {
+            name: 'Array',
+            check(value) {
+                return Array.isArray(value);
+            },
+            message(value) {
+                if (this.check(value)) {
+                    return `${value} is array`
+                } else {
+                    return `${value} is not array`
+                }
+            }
+        },
+        string = {
+            name: 'String',
+            check(value) {
+                return typeof value === 'string';
+            },
+            message(value) {
+                if (this.check(value)) {
+                    return `${value} is string`
+                } else {
+                    return `${value} is not a string`
+                }
+            }
+        },
+        criteriaArr = [number, array, string];
+
     let validObj = new Validation(criteriaArr);
-    validObj.toggleValidator('Array');
-    validObj.toggleValidator('Array');
-    validObj.toggleValidator('Array');
-    validObj.toggleValidator('Number');
-    validObj.toggleValidator('Number');
+        validObj.toggleValidator('Number');
+        validObj.toggleValidator('Array');
 
-
-
-
-
-    console.log(validObj.customCriteria);
 
 }());
